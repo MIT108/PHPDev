8195ce1aa63f91cc788ba8461fbad106d5e874c7
NdelogakehDaniel <ndabosed@gmail.com>
2022-07-11 12:38:53 +0100

    Added function to get all elements from a given table

--- Controller/backend.php
+++ Controller/backend.php
@@ -4,17 +4,24 @@ require '../Database/connection.php';
 class Backend{
     private $db;
 
+    //constructor
     function __construct(){
         $this->db = connect_to_db();
     }
 
-    //function to greet user\
-    function hello(){
-        echo "\nHello Users !!!\n";
+    //function to get all data
+    function findAll($tablename){
+        $data = $this->db->query("select * from ".$tablename."");
+        $arr = Array();
+        foreach ($data as $row){
+            array_push($arr,$row);
+        }
+
+        return $arr;
     }
 }
 
 $b = new Backend();
-$b->hello();
+$b->findAll('users');
 
 ?>
--- Database/connection.php
+++ Database/connection.php
@@ -2,12 +2,17 @@
 
 function connect_to_db(){
     try{
-        $pdo = new PDO("mysql:host=localhost;dbname=bouy-task","root","");
+        $pdo = new PDO("mysql:host=localhost;dbname=phpdev","root","");
         return $pdo;
 
     }catch(PDOException $e){
         die("Database Error: ".$e->getMessage());
     }
 }
+
+// $pdo = new PDO("mysql:host=localhost;dbname=bouy-task","root","");
+// $db = $pdo;
+
+// $con = $db->query("select * from ")
 
 ?>
