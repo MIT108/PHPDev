a10c350f31f63df22ac7edfabc473b47c5f6ca50
iaifrns <nsinifranc@gmail.com>
2022-07-12 11:47:03 +0100

    puting try and catch in the select functions

--- Controller/backend.php
+++ Controller/backend.php
@@ -9,60 +9,80 @@ class Backend{
         $this->db = connect_to_db();
     }
 
+    
+    //=====================================================================================================//
+//*******************************  Beginning of select functions  **************************************/
+//=====================================================================================================//
+
     //function to get all data or get a rows
     //the id is optional here
     function findAll($tableName, $key=null, $value= null){
-        if(is_null($value)){
-            $data = $this->db->query("select * from ".$tableName."");
-        }else{
-            $data = $this->db->query("select * from ".$tableName." where ".$key."=".$value."");
+        try{
+            if(is_null($value)){
+                $data = $this->db->query("select * from ".$tableName."");
+            }else{
+                $data = $this->db->query("select * from ".$tableName." where ".$key."=".$value."");
+            }
+            
+            $arr = Array();
+            foreach ($data as $row){
+                array_push($arr,$row);
+            }
+    
+            return $arr;
+        }catch(Exception $e){
+            return $this->returnExecQueryMessage("Unexpected server error could not fine the data", false);
         }
-        
-        $arr = Array();
-        foreach ($data as $row){
-            array_push($arr,$row);
-        }
-
-        return $arr;
+       
     }
 
     //function to select one or more column in the database
     function findOne($tableName, $columnName, $key=null, $value= null){
         $set = Array(); 
-        for($i=0; $i<count($columnName); $i++){
-            if(is_null($value)){
-                $data = $this->db->query("select ".$columnName[$i]." from ".$tableName."");    
-            }else{
-                $data = $this->db->query("select ".$columnName[$i]." from ".$tableName." where ".$key."=".$value."");
+        try{
+            for($i=0; $i<count($columnName); $i++){
+                if(is_null($value)){
+                    $data = $this->db->query("select ".$columnName[$i]." from ".$tableName."");    
+                }else{
+                    $data = $this->db->query("select ".$columnName[$i]." from ".$tableName." where ".$key."=".$value."");
+                }
+                
+                foreach ($data as $row){
+                    array_push($set,$row);
+                }
             }
             
-            foreach ($data as $row){
-                array_push($set,$row);
-            }
+            return $set;
+        }catch(Exception $e){
+            return $this->returnExecQueryMessage("Unexpected server error could not fine the data", false);
         }
         
-        return $set;
     }
 
     //function to verify if a table exit
     function checkTables($tableName){
-        $data=$this->db->query("show tables");
-        $arr=Array();
-        $i=0;
-        $t=false;
-
-        foreach($data as $row){
-            array_push($arr,$row);
-            $i++;
-        }
-
-        for($j=0; $j<$i; $j++){
-            if($tableName==$arr[$j][0]){
-                $t=true;
+        try{
+            $data=$this->db->query("show tables");
+            $arr=Array();
+            $i=0;
+            $t=false;
+    
+            foreach($data as $row){
+                array_push($arr,$row);
+                $i++;
             }
+    
+            for($j=0; $j<$i; $j++){
+                if($tableName==$arr[$j][0]){
+                    $t=true;
+                }
+            }
+            
+            return $t;
+        }catch(Exception $e){
+            return $this->returnExecQueryMessage("An Error occured doing verification", false);
         }
-        
-        return $t;
+       
     }
 
     //=====================================================================================================//
